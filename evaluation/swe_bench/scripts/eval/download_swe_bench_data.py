527f38f6405e0aafe7f3d3340af0d1d001d9c481
Xingyao Wang <xingyao6@illinois.edu>
2024-05-27 22:55:27 +0800

    pre-load P2P and F2P

--- evaluation/swe_bench/scripts/eval/download_swe_bench_data.py
+++ evaluation/swe_bench/scripts/eval/download_swe_bench_data.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 
 from datasets import load_dataset
 
@@ -13,8 +14,12 @@ args = parser.parse_args()
 
 dataset = load_dataset('princeton-nlp/SWE-bench')
 test = dataset['test'].to_pandas()
+test['PASS_TO_PASS'] = test['PASS_TO_PASS'].apply(json.loads)
+test['FAIL_TO_PASS'] = test['FAIL_TO_PASS'].apply(json.loads)
 test.to_json(f'{args.output_dir}/swe-bench-test.json', orient='records')
 
 dataset = load_dataset('princeton-nlp/SWE-bench_Lite')
 test = dataset['test'].to_pandas()
+test['PASS_TO_PASS'] = test['PASS_TO_PASS'].apply(json.loads)
+test['FAIL_TO_PASS'] = test['FAIL_TO_PASS'].apply(json.loads)
 test.to_json(f'{args.output_dir}/swe-bench-test-lite.json', orient='records')
